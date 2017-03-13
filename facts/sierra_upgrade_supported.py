936edcf813b90481e9cc0737d15901b2d17c05fc
Greg Neagle <gregneagle@mac.com>
2017-03-13 14:38:25 -0700

    get_board_id() was returning a string with an ending null byte (0)

--- facts/sierra_upgrade_supported.py
+++ facts/sierra_upgrade_supported.py
@@ -132,7 +132,7 @@ def is_supported_system_version():
 
 def get_board_id():
     '''Returns our board-id'''
-    return io_key("board-id").bytes().tobytes()
+    return io_key("board-id").bytes().tobytes()[0:-1]
 
 
 def is_supported_board_id():
