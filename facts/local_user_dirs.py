f94966c95b39b38354ac9150d7dbb9d1c8477bb4
Elliot Jordan <homebysix@users.noreply.github.com>
2018-04-12 16:14:33 -0700

    Exclude Deleted Users folder from local users

--- facts/local_user_dirs.py
+++ facts/local_user_dirs.py
@@ -6,7 +6,7 @@ import os
 def fact():
     '''Return the list of user home directories under /Users'''
     # skip_names should include any directories you wish to ignore
-    skip_names = ['Shared', 'admin']
+    skip_names = ['Deleted Users', 'Shared', 'admin']
     user_dirs = [item for item in os.listdir('/Users')
                  if item not in skip_names and not item.startswith('.')]
     return {'local_user_dirs': user_dirs}
