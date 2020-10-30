5fce86eb2f8b71719d3b4fdf67e5103e70d200f4
Shea Craig <sheagcraig@gmail.com>
2020-10-30 09:39:25 -0400

    Prevent potential `AttributeError` for differing text types.
    During regular execution, the existing code was fine; `stdout` would be
    a bytes object, and thus the `decode` method would be available.
    However, if the try/except block catches `IOError` or `OSError`, the
    assigned `stdout` value is an `str`, which lacks `decode` (it only has
    `encode`), and would thus still cause the code to fail.

--- facts/sip_status.py
+++ facts/sip_status.py
@@ -9,12 +9,12 @@ def fact():
     '''Return the current SIP status for the startup disk'''
     try:
         proc = subprocess.Popen(['/usr/bin/csrutil', 'status'],
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, text="UTF-8")
         stdout, _ = proc.communicate()
     except (IOError, OSError):
         stdout = 'Unknown'
 
-    return {'sip_status': stdout.strip().decode('utf-8')}
+    return {'sip_status': stdout.strip()}
 
 
 if __name__ == '__main__':
